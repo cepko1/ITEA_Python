aeed4b069de65d6e049df74679b939595da8e54d
cepko1 <92533232+cepko1@users.noreply.github.com>
2022-02-18 00:27:42 +0200

    V.1

---
+++ main.py
@@ -0,0 +1,12 @@
+notes = {'01.01.2023' : ["Great all with NY",
+                         "Open new calendar",
+                         "Sleep to 11 a.m"],
+         '31.08.2023' : ["Prepare to 1st September"],
+         '24.12.2023' : ["Prepare presents",
+                         "Buy newyear tree"]}
+# print (notes['01.01.2023'])
+#while choise <>
+for n in notes.keys():
+    print(n)
+for n in notes.keys():
+    print(notes[n])
